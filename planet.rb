a659fdf4d56d88d28d1ecdb2b5f391291aac196d
ChelseaC13 <chelhelg@outlook.com>
2020-02-18 15:35:51 -0800

    Added summary instance method and applied it to main

--- planet.rb
+++ planet.rb
@@ -10,8 +10,9 @@ class Planet
     @fun_fact = fun_fact
   end
 
+  def summary
+   return "#{@name} is #{@color} and weighs #{@mass_kg}kgs, while being
+     #{@distance_from_the_sun_km} kms away from the sun. Little known fact
+      is that #{@fun_fact}!"
+  end
 end
-
-# earth = Planet.new('Earth', 'blue-green', 5.972e24, 1.496e8, 'Only planet known to support life')
-
-# puts earth.name
