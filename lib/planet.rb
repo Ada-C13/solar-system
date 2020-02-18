7efd106ddb3061939c08c2b2412929cb5183f5c6
Nikki Vaughan <vnikki13@gmail.com>
2020-02-18 15:58:04 -0800

    Finished wave 2 tasks

--- lib/planet.rb
+++ lib/planet.rb
@@ -1,19 +1,19 @@
 class Planet
 
-  attr_reader :name, :color, :mass_kg, :distance_from_sun_km, :fun_fact
+  attr_reader :name, :color, :mass_kg, :distance_from_sun_miles, :fun_fact
 
 
-  def initialize(name, color, mass_kg, distance_from_sun_km, fun_fact)
+  def initialize(name, color, mass_kg, distance_from_sun_miles, fun_fact)
     @name = name
     @color = color
     @fun_fact = fun_fact
-    mass_kg > 0 ? @mass_kg = mass_kg : raise(ArgumentError, "Mass is less than 0")
-    distance_from_sun_km > 0 ? @distance_from_sun_km = distance_from_sun_km : raise(ArgumentError, "Distance is less than 0")
+    mass_kg.to_i > 0 ? @mass_kg = mass_kg : raise(ArgumentError, "Mass is less than 0")
+    distance_from_sun_miles.to_i > 0 ? @distance_from_sun_miles = distance_from_sun_miles : raise(ArgumentError, "Distance is less than 0")
   end
 
   def summary
     return "
-    #{@name} is #{@distance_from_sun_km}km from the sun and weighs #{@mass_kg}kg.
+    #{@name} is #{@distance_from_sun_miles}km from the sun and weighs #{@mass_kg}kg.
     The #{@name}'s color is #{color} and a fun fact is: #{@fun_fact}
     "
   end
--- lib/solar_system.rb
+++ lib/solar_system.rb
@@ -10,7 +10,31 @@ class SolarSystem
   end
 
   def list_planets
-    puts "Planets orbiting #{@star_name}"
-    @planets.each_with_index {|planet, idx| return "#{idx + 1}. #{planet.name}"}
+    list = "Planets orbiting #{@star_name}: "
+    @planets.each_with_index do |planet, idx|
+      list += "\n#{idx + 1}. #{planet.name}"
+    end
+
+    return list
+  end
+
+  def find_planet_by_name(find_planet)
+    matching_planets = @planets.select { |current_planet| current_planet.name.casecmp(find_planet) == 0 }
+    if matching_planets.length == 1
+      return matching_planets.first
+    elsif matching_planets.length > 1
+      return "There are multiple planets with that name"
+    else
+      return "Theres no planets with that name"
+    end
+  end
+
+  def distance_between(planet1, planet2)
+    start_planet = find_planet_by_name(planet1)
+    end_planet = find_planet_by_name(planet2)
+
+    if start_planet.distance_from_sun_km > end_planet.distance_from_sun_km
+      puts "#{planet1} is further than #{planet2}"
+    end
   end
 end
