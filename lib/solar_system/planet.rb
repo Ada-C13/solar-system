5fe22d88337a2cfe5798797c8699c5fcf8782331
Nataliya Pogodina <n.pogodina@gmail.com>
2020-02-18 15:32:39 -0800

    Added guard closes for mass and distance.

--- lib/solar_system/planet.rb
+++ lib/solar_system/planet.rb
@@ -2,6 +2,9 @@ class Planet
   attr_reader :name, :color, :mass_kg, :distance_from_sun_km, :fun_fact
 
   def initialize(name, color, mass_kg, distance_from_sun_km, fun_fact)
+    raise ArgumentError.new("Planet's mass should be greater than zero.") unless mass_kg.respond_to?(:>) && mass_kg > 0 
+    raise ArgumentError.new("Distance from Sun should be greater than zero.") unless distance_from_sun_km.respond_to?(:>) && distance_from_sun_km > 0
+
     @name = name
     @color = color
     @mass_kg = mass_kg
