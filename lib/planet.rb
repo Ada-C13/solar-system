6bf39264daec1d680c367141d12dbc316f90efa3
vwhwang <vwhwang38@gmail.com>
2020-02-18 14:43:14 -0800

    wave 1 of solor completed

---
+++ lib/planet.rb
@@ -0,0 +1,24 @@
+require "pry"
+
+class Planet
+  
+  attr_reader :name, :color, :mass_kg, :distance_from_sun_km, :fun_fact
+  
+  def initialize(name,color,mass_kg,distance_from_sun_km,fun_fact)
+    @name = name
+    @color = color
+    @mass_kg = mass_kg
+    @distance_from_sun_km = distance_from_sun_km
+    @fun_fact = fun_fact
+    unless @mass_kg > 0 && @distance_from_sun_km > 0 
+      raise ArgumentError,'mass and distance must be greather than 0'
+    end 
+
+  end 
+
+  def summary
+    return "#{@name} is #{@color}. have a mass in kg of #{@mass_kg}.
+     The distance from sun is #{@distance_from_sun_km}km. #{@fun_fact}"
+  end 
+
+end 
