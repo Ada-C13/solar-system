1a11780db5f8064ff7074d6f9e918384bee725d2
ChelseaC13 <chelhelg@outlook.com>
2020-02-18 14:58:44 -0800

    created a constructor - wave 1 number 2

--- planet.rb
+++ planet.rb
@@ -1,3 +1,17 @@
 class Planet
 
+  attr_reader :name, :color, :mass_kg, :distance_from_the_sun_km, :fun_fact
+
+  def initialize(name, color,mass_kg, distance_from_the_sun_km, fun_fact)
+    @name = name
+    @color = color
+    @mass_kg = mass_kg
+    @distance_from_the_sun_km = distance_from_the_sun_km
+    @fun_fact = fun_fact
+  end
+
 end
+
+# earth = Planet.new('Earth', 'blue-green', 5.972e24, 1.496e8, 'Only planet known to support life')
+
+# puts earth.name
