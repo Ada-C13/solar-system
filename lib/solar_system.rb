97dff9058ac345da0863bd85b911f21fcabd1368
Nikki Vaughan <vnikki13@gmail.com>
2020-02-18 13:46:48 -0800

    Added a method to save relative planets and also a method to output those planets

--- lib/solar_system.rb
+++ lib/solar_system.rb
@@ -4,4 +4,13 @@ class SolarSystem
     @star_name = star_name
     @planets = []
   end
+
+  def add_planet(planet)
+    @planets << planet
+  end
+
+  def list_planets
+    puts "Planets orbiting #{@star_name}"
+    @planets.each_with_index {|planet, idx| return "#{idx + 1}. #{planet.name}"}
+  end
 end
