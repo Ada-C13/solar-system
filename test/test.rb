7cd86b8c5c403b6f53164b437e32ccf778512727
Jessica Stone <jstone4815@gmail.com>
2020-02-19 13:20:36 -0800

    add test for find_planet_by_name method

--- test/test.rb
+++ test/test.rb
@@ -3,6 +3,7 @@ require 'minitest/reporters'
 require 'minitest/skip_dsl'
 
 require_relative '../lib/planet'
+require_relative '../lib/solar_system'
 
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
@@ -14,6 +15,21 @@ describe 'Planet' do
       expect(earth.mass_kg).must_be :>, 0
       expect(earth.distance_from_sun_km).must_be :>, 0
     end
+  end
+end
 
+describe 'SolarSystem' do
+  describe 'find_planet_by_name method' do
+    it 'returns planet info when valid planet is provided' do
+      solar_system = SolarSystem.new('Sol')
+
+      earth = Planet.new('Earth', 'blue-green', 5.972e24, 1.496e8, 'Only planet known to support life')
+
+      solar_system.add_planet(earth)
+
+      earth_found = solar_system.find_planet_by_name('Earth')
+
+      expect(earth_found).must_equal earth.summary
+    end
   end
 end
