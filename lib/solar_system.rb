d5ae82341224376d7e229c108e4927a88c69d576
saraleecodes <sara.lee.higgins@gmail.com>
2020-02-18 16:01:09 -0800

    Wave 2: Create a method SolarSystem#list_planets

--- lib/solar_system.rb
+++ lib/solar_system.rb
@@ -9,6 +9,12 @@ class SolarSystem
 
 	# take an instance of Planet as a parameter and add it to the list of planets
 	def add_planet(planet)
-		@planets << planet.name
+		@planets << "#{@planet_num}. #{planet.name}"
+		@planet_num += 1
+	end
+
+	# return (not puts) a string containing a list of all the planets in the system
+	def list_planets
+		return "Planets orbiting #{@star_name}:\n #{@planets}"
 	end
 end
