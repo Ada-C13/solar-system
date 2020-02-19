6c6da4a3bf4c11972232bb187857a49f12063c42
vwhwang <vwhwang38@gmail.com>
2020-02-18 20:17:42 -0800

    fixed test with Solor system class

--- test/solar_system_test.rb
+++ test/solar_system_test.rb
@@ -11,8 +11,18 @@ Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 describe "solar system" do 
   it "return correct distance between earth and mercury" do 
+    earth = Planet.new('Earth', 'blue-green', 5.972e24, 1.496e8, 'Only planet known to support life')
+    solar_system = SolarSystem.new('Sol')
+    solar_system.add_planet(earth)
+    mercury = Planet.new('Mercury','blue',3.285e23,4.809e7,"closest to sun")
+    solar_system.add_planet(mercury)
     input = solar_system.distance_between('earth','mercury')
     expect(input).must_equal 101510000.0
+  end 
+
+  it "assign star name" do 
+    output = SolarSystem.new('star_fake')
+    expect(output.star_name).must_equal 'star_fake'
   end 
 end 
 
