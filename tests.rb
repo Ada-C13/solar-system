5678809c5f57563f6595e7fe074e7f0e5e354259
Alicia Combs <alicia.combs.92@gmail.com>
2020-02-19 12:02:34 -0800

    Updated Planet class to throw argumenterror when incorrect input, created tests file

--- tests.rb
+++ tests.rb
@@ -3,3 +3,26 @@ require 'minitest/reporters'
 require 'minitest/skip_dsl'
 
 require_relative 'main.rb'
+
+describe "Planet class constructor method" do
+  it "accepts valid input and requires all parameters" do
+    mercury = Planet.new('Mercury', 'yellow-blue', 3.285e23, 5.791e7, 'Mercury is the fasest planet. Completing a full circle around the sun in 88 days')
+  end
+
+
+
+
+
+end
+
+
+describe do
+  it do
+
+  end
+
+
+
+
+
+end
