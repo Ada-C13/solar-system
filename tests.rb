71890dba5f16dcb4d86151c0933ab8c51a61827f
Alicia Combs <alicia.combs.92@gmail.com>
2020-02-19 12:13:45 -0800

    Edit test file to check that planet constructer works as expected and throws an ArgumentError if invalid input is used

--- tests.rb
+++ tests.rb
@@ -6,15 +6,20 @@ require_relative 'main.rb'
 
 describe "Planet class constructor method" do
   it "accepts valid input and requires all parameters" do
+    #instantiate object with appropriate arguments
     mercury = Planet.new('Mercury', 'yellow-blue', 3.285e23, 5.791e7, 'Mercury is the fasest planet. Completing a full circle around the sun in 88 days')
+    #checks that mercury is planet class
+    expect(mercury.class).must_be_instance_of Planet
+    #checks attributes of mercury are accurate
+    expect(mercury.name).must_equal "Mercury"
   end
+  it "throws an Argument Error if invalid mass or distance is given" do
+    #instantiate object with inappropriate distance argument
+    expect(mercury = Planet.new('Mercury', 'yellow-blue', 3.285e23, "five million", 'Mercury is the fasest planet. Completing a full circle around the sun in 88 days')).must_raise ArgumentError
 
-
-
-
-
+    expect(mercury = Planet.new('Mercury', 'yellow-blue', "literally anything", 5.791e7, 'Mercury is the fasest planet. Completing a full circle around the sun in 88 days')).must_raise ArgumentError
+  end
 end
-
 
 describe do
   it do
