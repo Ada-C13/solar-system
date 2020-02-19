0e266b677f07359d16c21e506753d77a5760117c
Leah Hughes <leahwhughes@gmail.com>
2020-02-19 13:33:01 -0800

    refactoring main method, added user_adds_planet method

--- lib/main.rb
+++ lib/main.rb
@@ -1,6 +1,43 @@
 require_relative "planet.rb"
 require_relative "solar_system.rb"
 
+def user_wants_to(user_choice_parameter)
+  valid_choice = ["1", "1.", "list", "list planets", "2", "2.", "details", "get details", "get planet details", "3", "3.", "add", "add planet", "add a planet", "add a planet to the system", "add planet system", "4", "4.", "exit", "quit", "ex", "q"]
+  case user_choice_parameter
+  when "1", "1.", "list", "list planets"
+    return "list"
+  when "2", "2.", "details", "get details", "get planet details"
+    return "details"
+  when "3", "3.", "add", "add planet", "add a planet", "add a planet to the system", "add planet system"
+    return "add"
+  when "4", "4.", "exit", "quit", "ex", "q"
+    return "exit"
+  end 
+end
+
+def user_adds_planet
+  print "What is this planet's name? ==> "
+  name = gets.chomp
+  
+  print "\nWhat color is #{name}? ==> "
+  color = gets.chomp
+  
+  print "\nWhat is #{name}'s mass?"
+  mass = gets.chomp.to_f
+  
+  print "\nHow far is #{name} away from the Sun? ==> "
+  distance = gets.chomp.to_f
+  
+  print "\nWhat's a fun fact about #{name}? ==> "
+  fun_fact = gets.chomp
+  
+
+  user_planet = Planet.new(name.capitalize, color, mass, distance, fun_fact)
+
+  return user_planet
+end
+
+
 def main
   # instantiate solar system - add planets
   sun = SolarSystem.new("Sun")
@@ -10,26 +47,73 @@ def main
   jupiter = Planet.new("Jupiter", "orange & brown", 1.8982e27, 778000000, "has 79 known moons; its largest moon has a bigger diameter than Mercury")
   mercury = Planet.new("Mercury", "red", 0.330e24, 57e6, "has no moons 😢 ")
   
+  # add planets to Sun
   sun.add_planet(mars)
   sun.add_planet(jupiter)
   sun.add_planet(mercury)
   
-  puts "What would you like to do? List planets or exit?"
-  print "You can enter list or exit ==> "
+  puts "Welcome to our Super Solar System Simulator!"
+  puts "You've got a few choices ahead of you..."
+  
+  puts "\nHere are your choices:\n1. List planets\n2. Get planet details\n3. Add a planet to the system\n4. Exit"
   user_choice = gets.chomp.downcase
   
-  if user_choice == "list"
-    puts "#{sun.list_planets}"
-  elsif user_choice == "exit"
-    print "Ok, see you next time!"
-    exit
+  until user_choice == "exit"
+    user_choice = user_wants_to(user_choice)
+    
+    while user_choice == "list"
+      puts sun.list_planets
+      puts "\nHere are your choices:\n1. List planets\n2. Get planet details\n3. Add a planet to the system\n4. Exit"
+      user_choice = gets.chomp.downcase
+    end
+    
+    while user_choice == "details"
+      print "\nWhat planet do you want to know more about? ==> "
+      user_planet = gets.chomp
+      found_planet = sun.find_planet_by_name(user_planet)
+      puts found_planet.summary
+      
+      puts "\nHere are your choices:\n1. List planets\n2. Get planet details\n3. Add a planet to the system\n4. Exit"
+      user_choice = gets.chomp.downcase
+    end
+    
+    while user_choice == "add"
+      sun.add_planet(user_adds_planet)
+
+      puts "Cool! Thanks for adding that planet to our solar system!"
+
+      puts "\nHere are your choices:\n1. List planets\n2. Get planet details\n3. Add a planet to the system\n4. Exit"
+      user_choice = gets.chomp.downcase
+    end
+    
+    if user_choice == "exit"
+      puts "See you later!"
+      exit
+    end
+    
+    if user_choice == "invalid"
+      puts "I did not understand that choice..."
+      puts "See you later!"
+      exit
+    end
+
   end
-  # enter control loop that asks user what to do next
-  # list planets OR exit
-  
-  
   
 end
+
+
+
+
+
+# print "\nWhat planet should we add to our solar system? ==> "
+#       user_added_planet = gets.chomp.downcase
+#       user_added_planet = Planet.new(user_added_planet.capitalize) # how to pass the parameters here?
+#       sun.add_planet(user_added_planet)
+      
+#       puts "\nHere are your choices:\n1. List planets\n2. Get planet details\n3. Add a planet to the system\n4. Exit"
+#       user_choice = gets.chomp.downcase
+
+
 
 # def main
 
--- lib/planet.rb
+++ lib/planet.rb
@@ -1,22 +1,22 @@
 class Planet
-
+  
   attr_reader :name, :color, :mass_kg, :distance_from_sun_km, :fun_fact
-
+  
   def initialize(name, color, mass_kg, distance_from_sun_km, fun_fact)
-  @name = name
-  @color = color
-  @mass_kg = mass_kg
-  @distance_from_sun_km = distance_from_sun_km
-  @fun_fact = fun_fact
+    @name = name
+    @color = color
+    @mass_kg = mass_kg
+    @distance_from_sun_km = distance_from_sun_km
+    @fun_fact = fun_fact
     
-  # add error checking for numbers
-  if @mass_kg < 0 || @distance_from_sun_km < 0
-    raise(ArgumentError, "Mass or distance cannot be negative")
+    # add error checking for numbers
+    if @mass_kg < 0 || @distance_from_sun_km < 0
+      raise(ArgumentError, "Mass or distance cannot be negative")
+    end
   end
-end
-
+  
   def summary
     return "\nWhat do we know about #{name}?  \nIt is #{color}.  It has a mass of #{mass_kg} kilograms.  #{name} is #{distance_from_sun_km} kilometers from the Sun.  Did you know that #{name} #{fun_fact}?  Pretty fascinating, right?"
   end
-
+  
 end
