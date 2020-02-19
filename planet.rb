8f7b73c86f0ea21dc14222434efe5c5c2df69860
Alicia Combs <alicia.combs.92@gmail.com>
2020-02-19 11:53:12 -0800

    Implemented logic in Planet class so that constructor will only assign mass and distance values if they're a valid, positive number

--- planet.rb
+++ planet.rb
@@ -5,8 +5,8 @@ class Planet
   def initialize(name, color, mass_kg, distance_from_sun_km, fun_fact)
     @name = name
     @color = color
-    @mass_kg = mass_kg
-    @distance_from_sun_km = distance_from_sun_km
+    @mass_kg = mass_kg if mass_kg > 0
+    @distance_from_sun_km = distance_from_sun_km if distance_from_sun_km > 0
     @fun_fact = fun_fact
   end
 
