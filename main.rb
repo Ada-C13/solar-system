5c860c0cfa86c3d464c517f32fd4553d39a8cf51
Stephanie <stephaniejmars@gmail.com>
2020-02-18 16:01:28 -0800

    added solar system

--- main.rb
+++ main.rb
@@ -1,11 +1,23 @@
+
 require_relative 'planet'
+require_relative 'solar_system'
 
 def main
+  solar_system = SolarSystem.new('Sol')
   earth = Planet.new('Earth', 'blue-green', 5.972e24, 1.496e8, 'Only planet known to support life')
-
+  solar_system.add_planet(earth)
   mars = Planet.new('Mars', 'red', 5.972e24, 1.496e8, 'None of this is true for Mars')
-  
-  puts earth.name
+  solar_system.add_planet(mars)
+  list = solar_system.list_planets
+  # puts list
+
+  found_planet = solar_system.find_planet_by_name('Earth')
+
+  puts found_planet
+  # => #<Planet:0x00007fe7c2868ee8>
+
+  puts found_planet.summary
+  # => Earth is a blue-green planet ...
 end
 
 main
