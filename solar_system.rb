30635a294f9c64aa58e5126336a6ba0f238125be
ChelseaC13 <chelhelg@outlook.com>
2020-02-18 16:24:19 -0800

    wave 2 number 3! i think it is right?

--- solar_system.rb
+++ solar_system.rb
@@ -6,4 +6,14 @@ class SolarSystem
     @planets = []
   end
 
+  def add_planet(planet)
+    @planets << planet
+  end
+
+  def list_planets
+    return "Planets orbiting @star_name \n 
+    #{@planets.eachwith {|indiv_planet,i| "#{i+1}  #{indiv_planet}\n"}}"
+  end
+
 end
+
