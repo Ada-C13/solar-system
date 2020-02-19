27a37cf919873ac6a10addbd499acf82bcaa296c
Jessica Stone <jstone4815@gmail.com>
2020-02-18 16:15:56 -0800

    add code to run SolarSystem class methods

--- main.rb
+++ main.rb
@@ -1,12 +1,21 @@
 require_relative 'lib/planet.rb'
+require_relative 'lib/solar_system.rb'
 
 def main
-  earth = Planet.new('Earth', 'blue-green', 0, 1.496e8, 'Only planet known to support life')
+  solar_system = SolarSystem.new('Sol')
 
+  earth = Planet.new('Earth', 'blue-green', 0, 1.496e8, 'Only planet known to support life')
   jupiter = Planet.new('Jupiter', 'red', 8.543e34, 5.643e9, 'The largest planet')
 
-  puts earth.summary
-  puts jupiter.summary
+  solar_system.add_planet(earth)
+  solar_system.add_planet(jupiter)
+
+  list = solar_system.list_planets
+  puts list
+
+  found_planet = solar_system.find_planet_by_name('earth')
+
+  puts found_planet
 end
 
 main
