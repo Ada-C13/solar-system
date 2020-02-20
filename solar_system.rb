5d4b3887b28437552467c2e86a392ded1bcdbc80
Lola Llanes <mslourdesllanes@gmail.com>
2020-02-20 09:43:36 -0800

    updated solar_system.rb with find_planet def and list_planets def

--- solar_system.rb
+++ solar_system.rb
@@ -5,4 +5,32 @@ class SolarSystem
 	  @star_name = star_name
 	  @planets = []
 	end
-  end
+  
+	def add_planet(planet)
+    	@planets << planet
+  	end
+
+	def list_planets
+		planets_orbit = "Planets orbiting #{@star_name}."
+    	@planets.each_with_index do |planet, index|
+      	planets_orbit += "#{index + 1}: #{planet.name}"
+		end
+		return planets_orbit
+  	end
+
+	def find_planet_by_name(name)
+		found = false
+		@planets.each do |planet|
+			if planet.name.downcase == name.downcase
+				found = true
+				return planet
+			else 
+				found = false
+				end
+			end
+			if found == false 
+		  	return "This is not a planet!"
+			end
+	end
+end
+
