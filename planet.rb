760ae1d7d9409b76fb2621b28b8540430d5a9ebd
mulhoo <oliviamulholland94@gmail.com>
2020-02-19 21:00:43 -0800

    Wave 0 commit

---
+++ planet.rb
@@ -0,0 +1,16 @@
+class Planet 
+  attr_accessor :name, :color, :mass_kg, :distance_from_sun_km, :fun_fact
+
+  def initialize(name, color, mass_kg, distance_from_sun_km, fun_fact)
+    @name = name
+    @color = color
+    @mass_kg = mass_kg
+    @distance_from_sun_km = distance_from_sun_km
+    @fun_fact = fun_fact
+  end
+
+  
+end
+
+
+
