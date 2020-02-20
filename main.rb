6226e24d818b64a3048d7a25ae2905f337bcf1f7
Alex Robertson <arobertson335@gmail.com>
2020-02-19 18:36:44 -0800

    Added 'main' file. Created 'main' method. Inserted two instances of the Planet class.

---
+++ main.rb
@@ -0,0 +1,16 @@
+require_relative "planet"
+
+def main()
+
+  our_ss = SolarSystem.new("Sol")
+
+  mercury = Planet.new("Mercury", "dark gray", "3.285 × 10^23 kg", "48.091 million km", "Calvin from Calvin and Hobbes did a lightning talk about the planet Mercury.")
+
+  venus = Planet.new("Venus", "ochre", "4.867 × 10^24 kg", "107.73 million km", "One day on Venus lasts 243 days on Earth.")
+
+  
+  our_ss.add_planet(mercury)
+  our_ss.add_planet(venus)
+end
+
+main()
