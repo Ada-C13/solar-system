db1f4447eb0fa3e6b460d3f2146706a5ee352b71
Hannah <hannahjoo24@gmail.com>
2020-02-19 23:15:45 -0800

    Added 'change lines' feature

--- lib/planet.rb
+++ lib/planet.rb
@@ -5,19 +5,23 @@ class Planet
   attr_reader :name, :color, :mass_kg, :distance_from_sun_km, :fun_fact
 
   def initialize(name, color, mass_kg, distance_from_sun_km, fun_fact)
-
     if mass_kg.to_f <= 0 || distance_from_sun_km.to_f <= 0 
       raise ArgumentError.new("\n ==> Both <mass_kg> and <distance_from_sun_km> must be greater than zero.")
     end 
-    
+
     @name = name.capitalize 
     @color = color.downcase 
     @mass_kg = mass_kg.to_f 
     @distance_from_sun_km = distance_from_sun_km.to_f 
-    @fun_fact = fun_fact.capitalize
+    @fun_fact = fun_fact
   end 
 
   def summary 
+    # Change lines
+    if @fun_fact.length > 53
+      @fun_fact = "#{@fun_fact[0..53]}\n#{@fun_fact[54..@fun_fact.length - 1]}"
+    end 
+
     titles = ["Name", "Color", "Distance from sun", "Mass", "Fun fact"]
     contents = [@name.blue.bold, @color, "#{@distance_from_sun_km} km", "#{@mass_kg} kg", @fun_fact]
 
@@ -26,9 +30,6 @@ class Planet
         t << [title, contents[i]]
       end 
     end
-
     return table
   end 
-
-  
 end 
