0fc769d450a9a2b66da015bfcf3259c8609d85c7
mulhoo <oliviamulholland94@gmail.com>
2020-02-20 01:34:38 -0800

    End Wave 2, Begin Wave 3

--- main.rb
+++ main.rb
@@ -1,4 +1,5 @@
 require_relative 'planet'
+require_relative 'solar_system'
 
 def main 
   tatooine = Planet.new("Tatooine", "brown", 20, 100, "Home to Luke Skywalker")
@@ -6,7 +7,38 @@ def main
   coruscant = Planet.new("Coruscant", "black", 19, 300, "Was considered the most politcally important world in the galaxy at one point" )
   bespin = Planet.new("Bespin", "sunset", 10, 400, "A gas giant whose cities float in the clouds")
 
-  puts endor.summary
+  solar_system = SolarSystem.new('Vēzos')
+  solar_system.add_planet(tatooine)
+  solar_system.add_planet(endor)
+  solar_system.add_planet(coruscant)
+  solar_system.add_planet(bespin)
+
+  user_choice = nil
+  until user_choice = "exit"
+    puts "You can see the current list of planets in your solar system ('LIST PLANETS'), "
+    print "or see details on a specific planet ('DETAILS')."
+    puts "To add another planet, please type 'ADD PLANET'."
+    puts "To exit the program, please type 'EXIT'."
+    user_choice = gets.chomp.upcase
+
+    if user_choice = "LIST PLANETS"
+      list = solar_system.list_planets
+      puts list
+
+    elsif user_choice = "DETAILS"
+      puts "Which planet would you like to know more about?"
+      user_planet_search = gets.chomp.downcase
+      solar_system.find_planet_by_name(user_planet_search)
+
+    elsif user_choice = "ADD PLANET"
+
+
+    elsif user_choice = "EXIT"
+      puts "#{solar_system.star_name} is supernova-ing your system."
+
+    end
+
+
 end
 
 main
--- solar_system.rb
+++ solar_system.rb
@@ -1,5 +1,28 @@
 class SolarSystem 
-  
+  attr_reader :planets, :star_name
 
+  def initialize(star_name)
+    @star_name = star_name
+    @planets = []
+  end
+
+  def add_planet(planet)
+    @planets << planet
+    return planets
+  end
+
+
+  def list_planets
+    puts "Planets Orbiting #{star_name}: "
+    return @planets.each_with_index do |planet, i|
+      puts "##{i+1}. #{planet.name}"
+    end 
+  end
+
+  def find_planet_by_name(planet)
+    if @planets.include?(planet) == true
+      return planet
+    end
+  end
 
 end
