9db1456f27ce1f39e905bd8446ad2c28187658e1
Lola Llanes <mslourdesllanes@gmail.com>
2020-02-20 09:41:34 -0800

    final planet.rb with updated summary

--- planet.rb
+++ planet.rb
@@ -1,25 +1,19 @@
 class Planet
     attr_reader :name, :color, :mass_kg, :distance_from_sun_km, :fun_fact
 
-    def initalize(name:, color:, mass_kg:, distance_from_sun_km:, fun_fact:)
-        @name = name
+    def initalize(name, color, mass_kg, distance_from_sun_km, fun_fact)
+        @name = name.upcase
         @color = color
         @mass_kg = mass_kg
         @distance_from_sun_km = distance_from_sun_km
         @fun_fact = fun_fact
     end
-
-
-    def color=(new_color)
-        @color=new_color
-    end
-
+    
     def summary
-        puts "The planet #{@name}'s color is #{@color} and weights #{@mass_kg} kgs. 
+        planet_details =
+        "The planet #{@name}'s is #{@color} in color and weighs #{@mass_kg} kgs. 
         #{@name}'s distance frome the sun is #{@distance_from_sun_km}. 
-        Here's a nugget about #{@name}; #{@fun_fact}."
+        Here's a fun nugget about #{@name}: #{@fun_fact}."
+        return planet_details
     end
 end
-
-
-
