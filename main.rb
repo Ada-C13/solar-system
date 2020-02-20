890639c4e6913ac4981b8ce32a39e2526e612567
Sharon Cheung <keikei1128@g.ucla.edu>
2020-02-19 20:31:49 -0800

    added find_planet_by_name & distance_between method

--- main.rb
+++ main.rb
@@ -5,11 +5,16 @@ require 'awesome_print'
 def main
   solar_system = SolarSystem.new('Sol')
   earth = Planet.new('Earth', 'blue-green', 5.972e24, 1.496e8, 'Only planet known to support life')
-  mars = Planet.new('Mars', 'red', 100, 200, "It's red!!!")
+  mars = Planet.new('Mars', 'red', 6.4171e23, 2.279e8, "There was water????")
+  uranus = Planet.new('Uranus', 'light-blue', 8.6810e25, 2.871e8, 'It turns on its axis once every 17 hours, 14 minutes')
   solar_system.add_planet(earth)
   solar_system.add_planet(mars)
   list = solar_system.list_planets
   puts list
+  found_planet = solar_system.find_planet_by_name('Earth')
+  puts found_planet
+  puts found_planet.summary
+  solar_system.distance_between('Earth', 'Mars')
 end 
 
 main
--- solar_system.rb
+++ solar_system.rb
@@ -8,16 +8,27 @@ class SolarSystem
   end
 
   def add_planet(planet)
-    @planets << planet.name
+    @planets << planet
+    p @planets
+    p @planets[0].name
   end
 
   def list_planets
-   list_planets = "Planets orbiting #{@star_name}:"
-   @planets.each.with_index do |planet, index|
-     list_planets += "\n#{index+1}. #{planet}" 
-    end
- 
+    list_planets = "Planets orbiting #{@star_name}:"
+    @planets.each.with_index do |planet, index|
+     list_planets += "\n#{index+1}. #{planet.name}" 
+    end 
     return list_planets
   end
-end
 
+
+  def find_planet_by_name(planet_name)
+    return @planets.select{|planet| planet_name.upcase == planet.name.upcase}[0]
+  end  
+  
+  def distance_between(planet1, planet2)
+    distance1 = find_planet_by_name(planet1).distance_from_sun_km
+    distance2 = find_planet_by_name(planet2).distance_from_sun_km
+    return (distance1 - distance2).abs
+  end
+end
